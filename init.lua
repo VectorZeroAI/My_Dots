07bc874a296834138e8d0b53de9388f2b4528326
Null <vectorzeroai@gmail.com>
2025-11-16 20:18:35 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- basic options
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.wrap = false
+
+vim.opt.numberwidth = 4
+vim.opt.cursorline = true
+
+vim.o.ignorecase = true
+
+-- auto center stuff
+
