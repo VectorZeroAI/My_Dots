277e4e6b01c494572d7f414c15dbab2eb05fa092
Null <vectorzeroai@gmail.com>
2025-11-18 15:47:40 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -13,20 +13,20 @@ vim.o.ignorecase = true
 -- auto center stuff
 
 vim.api.nvim_create_autocmd({"CursorMoved", "CursorMovedI"}, {
-		callback = funktion()
-				local win = 0
+	callback = funktion()
+		local win = 0
 				local view = vim.fn.winsaveview()
 
-				-- compute new topline so cursor stays centeered vertically
-				local height = vim.api.nvim_win_get_height(win)
-				local cursor_line = view.lnum
-				local new_top = cursor_line - math.floor(height / 2)
+		-- compute new topline so cursor stays centeered vertically
+		local height = vim.api.nvim_win_get_height(win)
+		local cursor_line = view.lnum
+		local new_top = cursor_line - math.floor(height / 2)
 				
-				if new_top < 1 then new_top = 1 end
+		if new_top < 1 then new_top = 1 end
 
-				view.topline = new_top
-				vim.fn.winrestview(view)
-		end, 
+		view.topline = new_top
+		vim.fn.winrestview(view)
+	end, 
 
 })
 
