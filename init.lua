544f968cc1a850e36e362d7c738c53a6ab221762
Null <vectorzeroai@gmail.com>
2026-01-02 14:16:17 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -351,3 +351,10 @@ vim.keymap.set("n", "<leader>f", function()
     })
     vim.lsp.buf.format()
 end, { desc = "Fix + format" })
+
+vim.keymap.set(
+  { "n", "v" },
+  "<leader>ca",
+  vim.lsp.buf.code_action,
+  { desc = "Code actions" }
+)
