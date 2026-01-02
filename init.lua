d0e2c52508bc1f9dd1ecfcb82aaca20ab6f1aa74
Null <vectorzeroai@gmail.com>
2026-01-02 10:04:27 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -56,7 +56,7 @@ vim.opt.rtp:prepend(lazypath)
             "shaunsingh/nord.nvim",
             priority = 1000,
             config = function()
-                vim.g.nord_contrast = false
+                vim.g.nord_contrast = true
                 vim.g.nord_borders = true
                 vim.g.nord_disable_background = false
                 vim.g.nord_cursorline_transparent = false
