7abaac6d4e556ba8ebbd22ad2874b50ae17d5f4d
Null <VectorZeroAI@gmail.com>
2026-01-03 18:06:22 +0100

    update init.lua

--- init.lua
+++ init.lua
@@ -65,6 +65,27 @@ vim.opt.rtp:prepend(lazypath)
                 vim.cmd.colorscheme("nord")
             end,
         },
+        {
+            'nvim-telescope/telescope.nvim',
+            dependencies = { 'nvim-lua/plenary.nvim' },
+            cmd = 'Telescope',
+            keys = {
+                { '<leader>sf', '<cmd>Telescope find_files<cr>', desc = '[S]earch [F]iles' },
+                { '<leader>sg', '<cmd>Telescope live_grep<cr>', desc = '[S]earch by [G]rep' },
+                
+                -- Override / to use Telescope
+                { '/', '<cmd>Telescope current_buffer_fuzzy_find<cr>', desc = 'Search buffer' },
+            },
+            config = function()
+                require('telescope').setup({
+                    defaults = {
+                        layout_strategy = 'horizontal',
+                        layout_config = { prompt_position = 'top' },
+                        sorting_strategy = 'ascending',
+                    },
+                })
+            end,
+        },
 
         -- AI diagnostics
         {
