8fc75582e7bddead3c4887f5de9a6d3cb02979b2
Null <VectorZeroAI@gmail.com>
2026-01-04 16:31:15 +0100

    fixed intebdation of blink.nvim plugin.

--- init.lua
+++ init.lua
@@ -332,26 +332,26 @@ vim.opt.rtp:prepend(lazypath)
     -- ============================================================
     -- AUTOCOMPLETION - BLINK.NVIM
     -- ============================================================
-    {
-        'saghen/blink.cmp',
-        dependencies = 'rafamadriz/friendly-snippets',
-        version = '*',
-        opts = {
-            keymap = { 
-                preset = 'default',
-                ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-                ['<CR>'] = { 'accept', 'fallback' },
+        {
+            'saghen/blink.cmp',
+            dependencies = 'rafamadriz/friendly-snippets',
+            version = '*',
+            opts = {
+                keymap = {
+                    preset = 'default',
+                    ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+                    ['<CR>'] = { 'accept', 'fallback' },
+                },
+                appearance = {
+                    use_nvim_cmp_as_default = true,
+                    nerd_font_variant = 'mono'
+                },
+                sources = {
+                    default = { 'lsp', 'path' },
+                },
+                signature = { enabled = true }
             },
-            appearance = {
-                use_nvim_cmp_as_default = true,
-                nerd_font_variant = 'mono'
-            },
-            sources = {
-                default = { 'lsp', 'path' },
-            },
-            signature = { enabled = true }
+            opts_extend = { "sources.default" }
         },
-        opts_extend = { "sources.default" }
-    },
 })
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
