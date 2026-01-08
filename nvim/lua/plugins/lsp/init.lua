9b4d3d546c57371a0563e525661ace747bf73886
Null <VectorZeroAI@gmail.com>
2026-01-08 17:46:56 +0100

    Fixed lua syntax.

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,14 +1,13 @@
 local is_android = vim.fn.has("android") == 1
-if is_android == true{
+
+if is_andoird then
     return {
-        require('plugins.lsp.mason'),
         require('plugins.lsp.lsp'),
     }
-}
-else{
+else
     return {
         require('plugins.lsp.mason'),
         require('plugins.lsp.lsp'),
         require('plugins.lsp.mason-lspconfig'),
     }
-}
+end
