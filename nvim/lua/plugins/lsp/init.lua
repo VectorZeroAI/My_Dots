5bfb869aa97d7015e17df907e3b5f8a522f0dd16
Null <VectorZeroAI@gmail.com>
2026-01-08 17:42:43 +0100

    disabled mason-lspconfig because its broken on android

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,5 +1,14 @@
-return {
-    require('plugins.lsp.mason'),
-    require('plugins.lsp.lsp'),
-    require('plugins.lsp.mason-lspconfig'),
+local is_android = vim.fn.has("android") == 1
+if is_android == true{
+    return {
+        require('plugins.lsp.mason'),
+        require('plugins.lsp.lsp'),
+    }
+}
+else{
+    return {
+        require('plugins.lsp.mason'),
+        require('plugins.lsp.lsp'),
+        require('plugins.lsp.mason-lspconfig'),
+    }
 }
