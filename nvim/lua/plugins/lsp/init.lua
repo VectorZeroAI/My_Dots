54b28ba24a192d1ef443627aea41196df88797af
Null <VectorZeroAI@gmail.com>
2026-01-08 20:10:56 +0100

    Fixing the mason-lspconfig shit

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,13 +1,17 @@
 local is_android = vim.fn.has("android") == 1
 
-if is_andoird then
-    return {
-        require('plugins.lsp.lsp'),
-    }
+local plugins
+
+if is_android then
+  plugins = {
+    require("plugins.lsp.lsp"),
+  }
 else
-    return {
-        require('plugins.lsp.mason'),
-        require('plugins.lsp.lsp'),
-        require('plugins.lsp.mason-lspconfig'),
-    }
+  plugins = {
+    require("plugins.lsp.mason"),
+    require("plugins.lsp.lsp"),
+    require("plugins.lsp.mason-lspconfig"),
+  }
 end
+
+return plugins
